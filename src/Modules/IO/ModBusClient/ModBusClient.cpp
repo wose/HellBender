d84b095c534238d4ef0c9b71698130fb5ea86c6f
Sebastian Woetzel <wose@zuendmasse.de>
2016-01-02 18:00:46 +0100

    added license comments

--- src/Modules/IO/ModBusClient/ModBusClient.cpp
+++ src/Modules/IO/ModBusClient/ModBusClient.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2016 The HellBender Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "IO/ModBusClient/ModBusClient.h"
 
 namespace hellbender {
--- src/Modules/IO/ModBusClient/ModBusClient.h
+++ src/Modules/IO/ModBusClient/ModBusClient.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2016 The HellBender Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef HB_MODULES_IO_MODBUSCLIENT_MODBUSCLIENT_H
 #define HB_MODULES_IO_MODBUSCLIENT_MODBUSCLIENT_H
 
@@ -9,7 +13,7 @@ class ModBusClient
  public:
     ModBusClient();
 
-    bool isConnected() { return connected_; }
+    bool isConnected() const { return connected_; }
  private:
     bool connected_ = false;
 
